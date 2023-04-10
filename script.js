34e8c25394615e5d170ed0064e25b40df994c3e4
Ola <hollabodey.kilaso@gmail.com>
2023-04-10 23:54:50 +0100

    Completed design

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Connected!");
+
+let dice = document.querySelector(".dice");
+
+
+$(".dice").click(function(e){
+    e.preventDefault();
+    console.log("clicked!");
+    fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((data) => {
+    document.querySelector(".myAdvice").innerText = data.slip.advice;
+    document.querySelector("#number").innerText = data.slip.id;
+    });
+})
+
+$("#fa").click(function(e){
+    e.preventDefault();
+    $("#body").toggleClass("light");
+    console.log("Hello LIGHT!");  
+})
